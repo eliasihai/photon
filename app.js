0d418b7345515d0b2111e8976d57595dd4d9f66e
Hai Eliasi <eliasihai@hotmail.com>
2020-05-20 01:59:59 +0300

    section 4.1

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
-const auth = '563492ad6f9170000100000176103c31ed3d49e4ad294c03b6f0ee70';
-const gallery = document.querySelector('.gallery');
-const searchInput = document.querySelector('.search-input');
-const submitBtn = document.querySelector('.submit-btn');
-const moreBtn = document.querySelector('.more');
+/** @format */
+
+// import { createClient } from "pexels";
+// const client = createClient(
+//   "563492ad6f9170000100000176103c31ed3d49e4ad294c03b6f0ee70",
+// );
+const auth = "563492ad6f9170000100000176103c31ed3d49e4ad294c03b6f0ee70";
+const gallery = document.querySelector(".gallery");
+const searchInput = document.querySelector(".search-input");
+const submitBtn = document.querySelector(".submit-btn");
+const moreBtn = document.querySelector(".more");
 let searchValue;
 
-
 async function curatedPhotos() {
-    const dataFetch = await fetch("https://api.pexels.com/v1/curated?per_page=1", {
-        method:
-    })
+    const dataFetch = await fetch(
+        "https://cors-anywhere.herokuapp.com/https://api.pexels.com/v1/curated?per_page=15&page=1", {
+            method: "GET",
+            headers: {
+                Accept: "application/json",
+                Authorization: auth,
+            },
+        },
+    );
+    const data = await dataFetch.json();
+    console.log(data);
 }
+
+curatedPhotos();
